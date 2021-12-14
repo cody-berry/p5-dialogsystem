0b7171bfd417bd08d4a7c014d1526669db62955c
Cody <cody.tian@icloud.com>
2021-12-14 12:25:10 -0800

    add version comments, WEBGL, easyCam, text frame

--- dialogbox.js
+++ dialogbox.js
@@ -1,5 +1,19 @@
 class DialogBox {
-    constructor(passages, highlightIndices, msPerPassage) {
-        
+    constructor(passages, highlightIndices, msPerPassage, textFrame) {
+        // let's assign the passages we have, the highlight indices given in
+        // a tuple, and the milliseconds per passage!
+        this.passages = passages
+        this.highlightIndices = highlightIndices
+        this.msPerPassage = msPerPassage
+        // we can also load the text frame
+        this.textFrame = textFrame
+        this.textFrame.resize(640, 360)
+    }
+
+    // loads the saved box texture with transparency
+    renderTextFrame(cam) {
+        cam.beginHUD(p5._renderer, width, height)
+        image(this.textFrame, 0, 0, width, height)
+        cam.endHUD()
     }
 }
--- sketch.js
+++ sketch.js
@@ -1,11 +1,24 @@
 /*
 
-@author 
-@date 2021-12-
+@author Cody
+@date 2021-12-6
+
+version comments
+    ... text list, highlight list, milliseconds per passage
+    .   display frame
+    ..  WEBGL and display it in the same place but with beginHUD() and endHUD()
+    *** blender axis
+    *   p5.easyCam()
+    *   display a single line inside the text frame
+    *   word wrap
+    *   advancing characters
+    *   advancing passages using some time delay system
+    *   SQUIRREL!!!
 */
 
 let font
 let passages // our json file input
+let dialogBox // our dialog box
 
 function preload() {
     font = loadFont('data/notjustgroovy.ttf')
@@ -17,10 +30,13 @@ function preload() {
 let textList = []
 /* grab other information: ms spent on each passage, highlights */
 let highlightList = [] // a list of tuples specifying highlights and indexes
-let msPerPassage = 0 // how long to wait before advancing a passage
+let msPerPassage = [] // how long to wait before advancing a passage
+let textFrame // our text frame
+let cam // our camera
 
 function setup() {
-    createCanvas(640, 360)
+    createCanvas(640, 360, WEBGL)
+    cam = new Dw.EasyCam(this._renderer, {distance: 240});
     colorMode(HSB, 360, 100, 100, 100)
     textFont(font, 14)
     for (let p in passages) {
@@ -30,17 +46,24 @@ function setup() {
         for (let h of passages[p]["highlightIndices"]) {
             highlightList.push([h["start"], h["end"]])
         }
-        msPerPassage = passages[p]["ms"]
-        console.log(msPerPassage)
+        msPerPassage.push(passages[p]["ms"])
+        // console.log(msPerPassage)
     }
+
     // console.log(highlightList)
     // console.log(passages.length)
     // console.log(textList)
+    textFrame = loadImage("data/textFrame.png")
+    // textFrame.resize(640, 360)
+    console.log(textFrame)
+    dialogBox = new DialogBox(passages, highlightList, msPerPassage, textFrame)
+    console.log(textFrame)
 }
 
 function draw() {
     background(234, 34, 24)
-
+    dialogBox.renderTextFrame(cam)
+    console.log(textFrame)
 
 }
 // prevent the context menu from showing up :3 nya~
